f668378a87581353243ba2d786b76586e0900775
Joe <pumphrey.joseph@gmail.com>
2018-10-29 14:13:06 -0700

    made a randomCustomerPerHour function.

---
+++ app.js
@@ -0,0 +1,14 @@
+// this function creates a random amount of customers
+function randomCustomerPerHour(min, max) {
+    return Math.floor(Math.random() * (50-1)+1);
+    console.log('number of customers', randomCustomerPerHour)
+};
+
+// 
+first_pike = {
+    hours: '6:00am-8:00pm',
+    minCustomers: ,
+    maxCustomers: ,
+    averageCookiesBought: ,
+
+};
